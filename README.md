f83ccd2551ae08513b6946f736fc1f75b5fd9373
Ricky Reusser <rsreusser@gmail.com>
2015-05-14 16:30:20 -0400

    Rework API to allow L & U in same matrix

--- README.md
+++ README.md
@@ -18,5 +18,16 @@ For an alternate version, see: [ndarray-lup-factorization](https://github.com/sc
 
 Inputs:
 - A: An n x n ndarray. This matrix is overwritten during the factorization. At the end of the factorization, the upper-triangular portion contains U and the lower-triangular portion contains zeros.
-- L: An n x n ndarray. At the end of the factorization, this array contains the lower-triangular portion of the factorization.
+- L: An n x n ndarray. At the end of the factorization, this array contains the lower-triangular portion of the factorization with ones on the diagonal.
 - P: An `Array`. At the end of the factorization, this contains a vector representation of the permutation matrix. The P[i]th element of the ith row of the permutation matrix is one; all others elements are zero.
+
+Returns:
+`true` upon successful completion; `false` otherwise.
+
+##### require('ndarray-lup-factorization-2')( A, A, P )
+
+- A: An n x n ndarray. If the first and second arguments are identical, both L and U are stored in the A matrix. U is the upper triangular portion (including diagonal) and L is the lower-triangular portion (excluding diagonal; ones on the diagonal are implicit).
+- P: An `Array`. At the end of the factorization, this contains a vector representation of the permutation matrix for which the P[i]th element of the ith row of the permutation matrix is one and all others elements are zero.
+
+Returns:
+`true` upon successful completion; `false` otherwise.
--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ var lup = require('../lib'),
     show = require('ndarray-show'),
     gemm = require('ndgemm')
 
-describe('lup',function() {
+describe('LUP',function() {
 
   var A, L, U, LU, P
 
@@ -22,25 +22,55 @@ describe('lup',function() {
     P = [0,1,2,3]
   })
 
-  it('factors a matrix',function() {
-    lup(A, L, P)
+  it('factors a matrix into L, U, and P',function() {
+    assert( lup.factorize(A, L, P), 'returns true on success')
 
-    var Aexp = ndarray([8,7,9,5, 0,7/4,9/4,17/4, 0,0,-6/7,-2/7, 0,0,0,2/3],[4,4])
+    var Aexp = ndarray([8,7,9,5, ,7/4,9/4,17/4, 0,0,-6/7,-2/7, 0,0,0,2/3],[4,4])
     var Lexp = ndarray([1,0,0,0, 3/4,1,0,0, 1/2,-2/7,1,0, 1/4,-3/7,1/3,1],[4,4])
     var Pexp = [2,3,1,0]
 
+    assert( ndt.approximatelyEqual( Aexp, A, 1e-8 ), 'A is correct' )
+    assert( ndt.approximatelyEqual( Lexp, L, 1e-8 ), 'L is correct' )
+    assert( ndt.approximatelyEqual( ndarray(Pexp), ndarray(P)), 'P is correct' )
 
-    console.log('L = \n'+show(L.transpose(1,0)))
-    console.log('U = \n'+show(A.transpose(1,0)))
-    console.log('P =',P)
+    //console.log('L = \n'+show(L.transpose(1,0)))
+    //console.log('U = \n'+show(A.transpose(1,0)))
+    //console.log('P =',P)
 
-    assert( ndt.approximatelyEqual( Aexp, A, 1e-8 ) )
-    assert( ndt.approximatelyEqual( Lexp, L, 1e-8 ) )
-    assert( ndt.approximatelyEqual( ndarray(Pexp), ndarray(P)) )
+    //gemm(LU,L,A)
 
-    gemm(LU,L,A)
+    //console.log(show(LU.transpose(1,0)))
 
-    console.log(show(LU.transpose(1,0)))
+    //console.log(ndarray(Float32Array([1,2,3,4]),[2,2]))
+
+  })
+
+  it('factors a matrix into LU and P',function() {
+    assert( lup.factorize(A, A, P), 'returns true on success')
+
+    //console.log('LU = \n'+show(A.transpose(1,0)))
+
+    var LUexp = ndarray([8,7,9,5, 3/4,7/4,9/4,17/4, 1/2,-2/7,-6/7,-2/7, 1/4,-3/7,1/3,2/3],[4,4])
+    var Pexp = [2,3,1,0]
+
+    assert( ndt.approximatelyEqual( LUexp, A, 1e-8 ), 'A is correct' )
+    assert( ndt.approximatelyEqual( ndarray(Pexp), ndarray(P)), 'P is correct' )
+
+  })
+
+  xit('solves Ax=b',function() {
+    var x = ndarray([13,38,102,107])
+    var bExp = ndarray([2,5,4,3])
+
+    assert( lup.noSwapFactorize(A, L, P), 'returns true on success')
+
+    console.log('A=\n'+show(A.transpose(1,0)))
+    console.log('L=\n'+show(L.transpose(1,0)))
+
+    lup.solve( L, A, P, x )
+
+    assert( ndt.approximatelyEqual( x, bExp ) )
+
 
   })
 })
