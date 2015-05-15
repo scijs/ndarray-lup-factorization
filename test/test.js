0c071c759ba84f937f42b67c3723ee98e961a989
Ricky Reusser <rsreusser@gmail.com>
2015-05-15 14:26:00 -0400

    Clean up cruft

--- test/test.js
+++ test/test.js
@@ -4,9 +4,7 @@ var lup = require('../lib'),
     pool = require('ndarray-scratch'),
     ndt = require('ndarray-tests'),
     ndarray = require('ndarray'),
-    assert = require('assert'),
-    show = require('ndarray-show'),
-    gemm = require('ndgemm')
+    assert = require('assert')
 
 describe('LUP',function() {
 
@@ -33,22 +31,13 @@ describe('LUP',function() {
     assert( ndt.approximatelyEqual( Lexp, L, 1e-8 ), 'L is correct' )
     assert( ndt.approximatelyEqual( ndarray(Pexp), ndarray(P)), 'P is correct' )
 
-    //console.log('L = \n'+show(L.transpose(1,0)))
-    //console.log('U = \n'+show(A.transpose(1,0)))
-    //console.log('P =',P)
-
-    //gemm(LU,L,A)
-
-    //console.log(show(LU.transpose(1,0)))
-
-    //console.log(ndarray(Float32Array([1,2,3,4]),[2,2]))
+    assert( ndt.matrixIsUpperTriangular( A, 1e-8 ), 'A is upper-triangular')
+    assert( ndt.matrixIsLowerTriangular( L, 1e-8 ), 'L is lower-triangular')
 
   })
 
   it('factors a matrix into LU and P',function() {
     assert( lup(A, A, P), 'returns true on success')
-
-    //console.log('LU = \n'+show(A.transpose(1,0)))
 
     var LUexp = ndarray([8,7,9,5, 3/4,7/4,9/4,17/4, 1/2,-2/7,-6/7,-2/7, 1/4,-3/7,1/3,2/3],[4,4])
     var Pexp = [2,3,1,0]
@@ -58,21 +47,5 @@ describe('LUP',function() {
 
   })
 
-  /*
-  xit('solves Ax=b',function() {
-    var x = ndarray([13,38,102,107])
-    var bExp = ndarray([2,5,4,3])
-
-    assert( lup.noSwapFactorize(A, L, P), 'returns true on success')
-
-    console.log('A=\n'+show(A.transpose(1,0)))
-    console.log('L=\n'+show(L.transpose(1,0)))
-
-    lup.solve( L, A, P, x )
-
-    assert( ndt.approximatelyEqual( x, bExp ) )
-
-
-  })*/
 })
 
